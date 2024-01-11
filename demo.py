a71a6644f1567bce3fef68ce7a51cdc9a6d8c996
daotrananhtuan09102002 <79463278+daotrananhtuan09102002@users.noreply.github.com>
2024-01-11 21:53:07 +0700

    first upload

---
+++ demo.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import utils
+
+from keras.preprocessing.image import img_to_array
+from PIL import Image
+
+
+def upload_image(img_file):
+    if img_file is not None:
+        img = Image.open(img_file)
+        img = img.resize((224, 224))
+        img = img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        return img
+    return None
+
+
+if __name__ == "__main__":
+    model = utils.load_model((224, 224, 3), 2, "./model.h5")
+
+    st.sidebar.title("Demo")
+    st.sidebar.subheader("Step 1: Upload your image")
+    img_file = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+    img = upload_image(img_file)
+    button = st.sidebar.button("Classify")
+
+    if button:
+        prediction = model.predict(img, verbose=0)
+        pred_class = np.argmax(prediction)
+        if pred_class == 0:
+            st.sidebar.subheader("Prediction: Cat")
+        else:
+            st.sidebar.subheader("Prediction: Dog")
+        
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+
+
+
+def load_model(input_shape, n_classes, model_path):
+    preprocess_input = tf.keras.applications.resnet50.preprocess_input
+    inputs = tf.keras.Input(shape=input_shape)
+    x = preprocess_input(inputs)
+
+    # Pretrained convolutional layers are loaded using the Imagenet weights.
+    # Include_top is set to False, in order to exclude the model's fully-connected layers.
+    conv_base = keras.applications.ResNet50(include_top=False,
+                     weights=None,
+                     input_shape=input_shape)
+
+
+    top_model = conv_base(x)
+    # Create a new 'top' of the model (i.e. fully-connected layers).
+    top_model = GlobalAveragePooling2D()(top_model)
+    output_layer = Dense(n_classes, activation='softmax')(top_model)
+
+    # Group the convolutional base and new fully-connected layers into a Model object.
+    model = Model(inputs=inputs, outputs=output_layer)
+    model.load_weights(model_path)
+
+    return model
+
